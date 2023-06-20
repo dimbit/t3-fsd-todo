462f24c081c1e6cd9eaf6933c4e6eab8b75f9154
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-20 12:44:32 +0100

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>To Do</title>
+        <meta name="description" content="To Do web app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>home</main>
+    </>
+  );
+}
