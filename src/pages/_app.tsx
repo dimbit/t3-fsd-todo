4c015e5c293ead396e9b6d188bb655c128caebac
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-20 17:26:22 +0100

    add public api to api module

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { type Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import { type AppType } from 'next/app'
-import { api } from '@/utils/api'
+import { api } from '@/shared/api'
 import '@/styles/globals.css'
 
 const MyApp: AppType<{ session: Session | null }> = ({
