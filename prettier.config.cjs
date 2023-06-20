7849d4e3575ddfa046162e985a91605bec84e728
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-20 14:33:57 +0100

    config prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,13 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-};
+	plugins: [require.resolve('prettier-plugin-tailwindcss')],
+	trailingComma: 'all',
+	tabWidth: 2,
+	semi: false,
+	singleQuote: true,
+	useTabs: true,
+	jsxSingleQuote: true,
+	singleAttributePerLine: true,
+}
 
-module.exports = config;
+module.exports = config
