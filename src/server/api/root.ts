af9b4e63aa95c8a483607aed487c940db1fcf3b5
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-20 14:34:04 +0100

    apply prettier rules

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,8 @@
-import { exampleRouter } from "@/server/api/routers/example";
-import { createTRPCRouter } from "@/server/api/trpc";
+import { exampleRouter } from '@/server/api/routers/example'
+import { createTRPCRouter } from '@/server/api/trpc'
 
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
-});
+	example: exampleRouter,
+})
 
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter
