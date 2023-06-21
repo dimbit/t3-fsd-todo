a8c4abf35b2a7a6813033f0dd31dfb188c99d4d8
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-21 16:16:34 +0100

    add list & kanban pages

---
+++ src/pages/kanban.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import { Layout } from '@/widgets/layout'
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>To Do</title>
+				<meta
+					name='description'
+					content='To Do web app'
+				/>
+				<link
+					rel='icon'
+					href='/favicon.ico'
+				/>
+			</Head>
+			<Layout>kanban</Layout>
+		</>
+	)
+}
