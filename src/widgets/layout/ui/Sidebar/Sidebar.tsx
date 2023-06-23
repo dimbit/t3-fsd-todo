5dd0eb899ea06e5908aac0d8aa5758926c8a4746
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-23 18:11:46 +0100

    add sign out button to sidebar

--- src/widgets/layout/ui/Sidebar/Sidebar.tsx
+++ src/widgets/layout/ui/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { ThemeToggleButton } from '@/features/theme-toggle'
 import { NavigationBar } from './NavigationBar'
+import { signOut } from 'next-auth/react'
 
 type Props = {
 	toggleCollapsed: () => void
@@ -8,6 +9,13 @@ type Props = {
 export const Sidebar = ({ isCollapsed, toggleCollapsed }: Props) => {
 	return (
 		<aside className={'flex h-full flex-col justify-between bg-slate-300'}>
+			<button
+				// eslint-disable-next-line @typescript-eslint/no-misused-promises
+				onClick={() => signOut()}
+			>
+				sign out
+			</button>
+
 			<div className={'flex flex-row-reverse justify-between'}>
 				<button onClick={toggleCollapsed}>
 					{isCollapsed ? 'show' : 'hide'}
