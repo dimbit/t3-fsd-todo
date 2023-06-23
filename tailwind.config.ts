b788e67ca5a9ab7634d27660e820577b35d8fc4b
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-23 18:13:35 +0100

    add some custom classNames for grid

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,12 @@ export default {
 			gridTemplateColumns: {
 				sidebar: '16rem 1fr',
 				'sidebar-collapsed': '3rem 1fr',
+				'auto-1fr': 'auto 1fr',
+				'1fr-auto': '1fr auto',
+			},
+			gridTemplateRows: {
+				'auto-1fr': 'auto 1fr',
+				'1fr-auto': '1fr auto',
 			},
 		},
 	},
