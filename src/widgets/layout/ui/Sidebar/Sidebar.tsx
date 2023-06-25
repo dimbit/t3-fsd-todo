79599b839f14d10c9b0713b635591526f443b133
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-25 14:54:48 +0100

    update sidebar styles

--- src/widgets/layout/ui/Sidebar/Sidebar.tsx
+++ src/widgets/layout/ui/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { ThemeToggleButton } from '@/features/theme-toggle'
-import { NavigationBar } from './NavigationBar'
+import { NavigationBar } from '@/features/navigation'
 import { signOut } from 'next-auth/react'
 import { ProfilePreview } from '@/entities/profile-preview'
 import clsx from 'clsx'
@@ -12,30 +12,50 @@ export const Sidebar = ({ isCollapsed, toggleCollapsed }: Props) => {
 	return (
 		<aside
 			className={
-				'grid h-full grid-rows-[auto_auto_auto_1fr_auto_auto_auto] overflow-hidden bg-white drop-shadow-sm dark:bg-neutral-700'
+				'grid h-full grid-rows-[auto_1fr_auto] overflow-hidden bg-white drop-shadow-sm dark:bg-neutral-700'
 			}
 		>
-			<ProfilePreview
-				className={clsx([
-					'p-4 transition-transform',
-					isCollapsed && '-translate-x-2',
-				])}
-			/>
-			<hr className={'dark:border-gray-500'} />
-			<div className={'flex flex-row-reverse justify-between'}>
+			<Section>
+				<ProfilePreview
+					className={clsx([
+						'p-2 transition-transform',
+						isCollapsed && '-translate-x-2',
+					])}
+				/>
+			</Section>
+			<Section>
+				<NavigationBar />
 				<button onClick={toggleCollapsed}>
 					{isCollapsed ? 'show' : 'hide'}
 				</button>
-			</div>
-			<NavigationBar />
-			<hr className={'dark:border-gray-500'} />
-			<ThemeToggleButton />
-			<button
-				// eslint-disable-next-line @typescript-eslint/no-misused-promises
-				onClick={() => signOut()}
-			>
-				Sign out
-			</button>
+			</Section>
+			<Section>
+				<ThemeToggleButton />
+				<button
+					// eslint-disable-next-line @typescript-eslint/no-misused-promises
+					onClick={() => signOut()}
+				>
+					Sign out
+				</button>
+			</Section>
 		</aside>
+	)
+}
+
+type SectionProps = {
+	children?: React.ReactNode
+	className?: string
+}
+const Section = ({ children, className }: SectionProps) => {
+	return (
+		<div
+			className={clsx([
+				'flex flex-col border-b border-b-neutral-300 p-2',
+				'last:border-b-0 dark:border-b-neutral-600',
+				className,
+			])}
+		>
+			{children}
+		</div>
 	)
 }
