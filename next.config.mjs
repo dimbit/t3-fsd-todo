87dae4a6b272fea3cd44f3339a7483c65fc782c2
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-25 14:14:56 +0100

    add svgr/webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,39 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
 await import('./src/env.mjs')
 
 /** @type {import("next").NextConfig} */
 const config = {
 	reactStrictMode: true,
+}
 
-	/**
-	 * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-	 * out.
-	 *
-	 * @see https://github.com/vercel/next.js/issues/41980
-	 */
-	i18n: {
-		locales: ['en'],
-		defaultLocale: 'en',
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**',
-			},
-		],
-	},
+config.i18n = {
+	locales: ['en'],
+	defaultLocale: 'en',
+}
+
+config.images = {
+	remotePatterns: [
+		{
+			protocol: 'https',
+			hostname: '**',
+		},
+	],
+}
+
+config.webpack = (config) => {
+	config.module.rules.push(
+		{
+			test: /\.svg$/i,
+			resourceQuery: /url/, // *.svg?url
+		},
+		{
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			resourceQuery: { not: /url/ }, // exclude if *.svg?url
+			use: [{ loader: '@svgr/webpack' }],
+		},
+	)
+
+	return config
 }
 
 export default config
