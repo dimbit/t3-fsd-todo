abe2387b241bb5cd099edb55de70445b9bda10ca
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-25 14:53:04 +0100

    mv nav bar to features & update its style

---
+++ src/features/navigation/ui/NavigationBar.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import { ROUTES } from '@/shared/routes'
+import KanbanIcon from '@/images/icons/kanban.svg'
+import ListIcon from '@/images/icons/list.svg'
+import clsx from 'clsx'
+
+const navigation = [
+	{
+		name: 'Kanban',
+		href: ROUTES.kanban.calcUrl(),
+		Icon: KanbanIcon as React.FC<React.SVGProps<SVGSVGElement>>,
+	},
+	{
+		name: 'List',
+		href: ROUTES.list.calcUrl(),
+		Icon: ListIcon as React.FC<React.SVGProps<SVGSVGElement>>,
+	},
+]
+
+export const NavigationBar = () => {
+	return (
+		<ul className={'grid grid-flow-row content-start'}>
+			{navigation.map(({ name, Icon, href }) => {
+				return (
+					<li key={name}>
+						<Link
+							href={href}
+							shallow
+							passHref
+							className={clsx([
+								'grid w-full grid-flow-col justify-start gap-2 rounded px-2 py-1 text-neutral-500',
+								'hover:bg-neutral-100 hover:text-current',
+							])}
+						>
+							{<Icon className={'stroke-current'} />}
+							{name}
+						</Link>
+					</li>
+				)
+			})}
+		</ul>
+	)
+}
