83d23fdaa184455e39944d0cc20ca212db68beca
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-26 17:41:55 +0100

    move task list page content to screen folder

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,29 +1,10 @@
-import Head from 'next/head'
-import { Layout } from '@/widgets/layout'
-import { TasksList } from '@/widgets/tasks-list'
 import type { GetServerSideProps } from 'next'
 import { getServerAuthSession } from '../server/auth'
 import { ROUTES } from '@/shared/routes'
+import { TaskListScreen } from '@/screens/task-list'
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>To Do</title>
-				<meta
-					name='description'
-					content='To Do web app'
-				/>
-				<link
-					rel='icon'
-					href='/favicon.ico'
-				/>
-			</Head>
-			<Layout>
-				<TasksList />
-			</Layout>
-		</>
-	)
+	return <TaskListScreen />
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
---
+++ src/screens/task-list/ui/TaskListScreen.tsx
@@ -0,0 +1,5 @@
+import { Layout } from '@/widgets/layout'
+
+export const TaskListScreen = () => {
+	return <Layout>task list</Layout>
+}
