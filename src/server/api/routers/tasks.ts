b99bd632291401628379b3d624c7c3218fecc027
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-26 18:04:49 +0100

    fix task endpoints

--- src/server/api/routers/tasks.ts
+++ src/server/api/routers/tasks.ts
@@ -1,19 +1,7 @@
 import { z } from 'zod'
-import {
-	createTRPCRouter,
-	publicProcedure,
-	protectedProcedure,
-} from '@/server/api/trpc'
+import { createTRPCRouter, protectedProcedure } from '@/server/api/trpc'
 
 export const tasksRouter = createTRPCRouter({
-	hello: publicProcedure
-		.input(z.object({ text: z.string() }))
-		.query(({ input }) => {
-			return {
-				greeting: `Hello ${input.text}`,
-			}
-		}),
-
 	getAll: protectedProcedure.query(async ({ ctx }) => {
 		const userId = ctx.session.user.id
 		const tasks = await ctx.prisma.task.findMany({
@@ -29,14 +17,13 @@ export const tasksRouter = createTRPCRouter({
 	getOne: protectedProcedure
 		.input(z.object({ id: z.string() }))
 		.query(async ({ ctx, input }) => {
-			const task = ctx.prisma.task.findUnique({
+			const userId = ctx.session.user.id
+			const task = ctx.prisma.task.findFirst({
 				where: {
 					id: input.id,
+					userId: userId,
 				},
 			})
 			return task
 		}),
-	getSecretMessage: protectedProcedure.query(() => {
-		return 'you can now see this secret message!'
-	}),
 })
