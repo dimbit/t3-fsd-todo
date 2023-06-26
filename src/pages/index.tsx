0016d0e0ac67271f6c6cf9db3b0f08500d77df6c
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-26 16:44:29 +0100

    move meta data from pages to _app.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { ROUTES } from '@/shared/routes'
 import type { GetServerSideProps } from 'next'
 import { getServerAuthSession } from '../server/auth'
@@ -7,17 +6,6 @@ import { SignInButton } from '@/features/authentication'
 export default function Home() {
 	return (
 		<>
-			<Head>
-				<title>To Do</title>
-				<meta
-					name='description'
-					content='To Do web app'
-				/>
-				<link
-					rel='icon'
-					href='/favicon.ico'
-				/>
-			</Head>
 			<div className={'flex h-full items-center justify-center'}>
 				<SignInButton />
 			</div>
