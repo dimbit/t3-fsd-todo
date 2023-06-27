352322490866430d1b9848a02c6b9ee2eac7d819
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-27 16:14:35 +0100

    add button and mutation to create a new task

--- src/widgets/tasks-list/ui/TasksList.tsx
+++ src/widgets/tasks-list/ui/TasksList.tsx
@@ -1,36 +1,49 @@
 import { TaskCard } from '@/entities/task-card'
 import { LoadingState } from '@/shared/ui-kit'
-import type { Task } from '@prisma/client'
+import type { Status, Task } from '@prisma/client'
+import { api } from '@/shared/api'
 
 type Props = {
 	tasks: Task[]
 	isLoading?: boolean
 	error?: string
-	status?: string
+	status: Status
 }
 export const TasksList = ({ tasks = [], isLoading, error, status }: Props) => {
+	const trpcUtils = api.useContext()
+	const addTaskMutation = api.tasks.createOne.useMutation({
+		onSuccess: async () => {
+			await trpcUtils.tasks.getAll.invalidate()
+		},
+	})
+
+	const handleMutation = () => {
+		addTaskMutation.mutate({
+			title: 'Created task',
+			description: 'Created task description',
+			statusId: status.id,
+		})
+	}
+
 	return (
 		<div className={'flex flex-col gap-2 p-4'}>
-			<h2 className={'px-4 font-bold'}>{status}</h2>
+			<h2 className={'px-4 font-bold'}>{status.name}</h2>
 			<div className={'flex flex-col'}>
 				<LoadingState
 					isLoading={isLoading}
 					error={error}
 				>
-					{tasks && tasks.length > 0 ? (
-						<div className={'flex flex-col gap-4'}>
-							{tasks?.map((task) => {
-								return (
-									<TaskCard
-										{...task}
-										key={task.id}
-									/>
-								)
-							})}
-						</div>
-					) : (
-						<span>nothing found</span>
-					)}
+					<div className={'flex flex-col gap-4'}>
+						{tasks?.map((task) => {
+							return (
+								<TaskCard
+									{...task}
+									key={task.id}
+								/>
+							)
+						})}
+					</div>
+					<button onClick={handleMutation}>add +</button>
 				</LoadingState>
 			</div>
 		</div>
