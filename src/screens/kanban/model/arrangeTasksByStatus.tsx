137388910a27392dda3b8c7dad9fde38dd77d64f
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-27 16:10:55 +0100

    change structure of returned data

--- src/screens/kanban/model/arrangeTasksByStatus.tsx
+++ src/screens/kanban/model/arrangeTasksByStatus.tsx
@@ -3,16 +3,23 @@ import type { Task, Status } from '@prisma/client'
 type TaskWithStatus = Task & {
 	status: Status
 }
+
 export const arrangeTasksByStatus = (tasks: TaskWithStatus[]) => {
 	return (
-		tasks?.reduce<{ [key: string]: Task[] }>((accumulator, task) => {
-			const status = task.status.name
-			if (!accumulator[status]) {
-				accumulator[status] = [task]
+		tasks?.reduce<{ status: Status; tasks: Task[] }[]>((accumulator, task) => {
+			const status = task.status
+			const existingEntityIndex = accumulator.findIndex(
+				(entity) => entity.status.id === status.id,
+			)
+			if (existingEntityIndex !== -1) {
+				accumulator[existingEntityIndex]?.tasks.push(task)
 			} else {
-				accumulator[status]?.push(task)
+				accumulator.push({
+					status,
+					tasks: [task],
+				})
 			}
 			return accumulator
-		}, {}) ?? {}
+		}, []) ?? []
 	)
 }
