e223c4863f0e097aa80efce222912a0c60485b50
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-28 15:11:27 +0100

    add task modal component

--- src/features/task-modal/index.ts
+++ src/features/task-modal/index.ts
@@ -1,1 +1,2 @@
+export { TaskModal } from './ui/TaskModal'
 export { useTaskModalStore } from './model'
---
+++ src/features/task-modal/ui/TaskModal.tsx
@@ -0,0 +1,39 @@
+import { Modal } from '@/entities/modal'
+import { useTaskModalStore } from '../model'
+import { api } from '@/shared/api'
+import { Button } from '@/shared/ui-kit'
+
+export const TaskModal = () => {
+	const { taskId, isOpen, closeModal } = useTaskModalStore()
+
+	const trpcUtils = api.useContext()
+	const allTasks = trpcUtils.tasks.getAll.getData()
+	const task = allTasks?.find((entity) => entity.id === taskId)
+
+	if (!task) {
+		return
+	}
+
+	return (
+		<Modal
+			isOpen={isOpen}
+			onClose={closeModal}
+		>
+			<form
+				onSubmit={console.log}
+				className={'flex flex-col gap-2'}
+			>
+				{task.title}
+				<input
+					name='title'
+					value={task.title}
+				/>
+				<textarea
+					name='description'
+					value={task.description ?? ''}
+				/>
+				<Button type='submit'>Save</Button>
+			</form>
+		</Modal>
+	)
+}
