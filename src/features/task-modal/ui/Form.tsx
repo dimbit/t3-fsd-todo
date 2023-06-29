1e86bad22831bc537b7d490efced784bedfb6006
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-06-29 19:15:53 +0100

    add some styles to inputs

--- src/features/task-modal/ui/Form.tsx
+++ src/features/task-modal/ui/Form.tsx
@@ -4,6 +4,7 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import type { SubmitHandler } from 'react-hook-form'
 import type { FormData } from '../model'
+import clsx from 'clsx'
 
 const mockedStatuses = [
 	{
@@ -36,11 +37,32 @@ export const Form = ({ title, description, statusId, onSubmit }: Props) => {
 	return (
 		<form
 			onSubmit={handleSubmit(onSubmit)}
-			className={'flex flex-col gap-2'}
+			className={'flex w-80 flex-col gap-4'}
 		>
-			<input {...register('title')} />
-			<textarea {...register('description')} />
-			<select {...register('statusId')}>
+			<input
+				className={clsx([
+					'rounded border px-4 py-2 shadow outline-none',
+					'border-neutral-200 bg-white dark:border-neutral-500 dark:bg-neutral-700',
+					'focus:bg-neutral-100 enabled:hover:bg-neutral-100 focus:dark:bg-neutral-600 enabled:dark:hover:bg-neutral-600',
+				])}
+				{...register('title')}
+			/>
+			<textarea
+				className={clsx([
+					'rounded border px-4 py-2 shadow outline-none',
+					'border-neutral-200 bg-white dark:border-neutral-500 dark:bg-neutral-700',
+					'focus:bg-neutral-100 enabled:hover:bg-neutral-100 focus:dark:bg-neutral-600 enabled:dark:hover:bg-neutral-600',
+				])}
+				{...register('description')}
+			/>
+			<select
+				className={clsx([
+					'cursor-pointer rounded border px-4 py-2 shadow outline-none',
+					'border-neutral-200 bg-white dark:border-neutral-500 dark:bg-neutral-700',
+					'focus:bg-neutral-100 enabled:hover:bg-neutral-100 focus:dark:bg-neutral-600 enabled:dark:hover:bg-neutral-600',
+				])}
+				{...register('statusId')}
+			>
 				{mockedStatuses.map((status) => {
 					return (
 						<option
