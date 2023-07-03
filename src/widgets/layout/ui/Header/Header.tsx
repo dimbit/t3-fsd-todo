d008c56e7c60f06087aa8beaf6e024e19c1a90f5
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-07-03 16:12:10 +0100

    add onClick  handler to add task button

--- src/widgets/layout/ui/Header/Header.tsx
+++ src/widgets/layout/ui/Header/Header.tsx
@@ -1,9 +1,16 @@
+import { useTaskModalStore } from '@/features/task-modal'
+
 import { Button } from '@/shared/ui-kit/Button'
 
 export const Header = () => {
+	const { openTaskCreationModal } = useTaskModalStore()
+
+	const handleClickAddTask = () => {
+		openTaskCreationModal({})
+	}
 	return (
 		<div className={'flex h-14 w-full items-center justify-end px-4'}>
-			<Button>Add task</Button>
+			<Button onClick={handleClickAddTask}>Add task</Button>
 		</div>
 	)
 }
