74a5c832068cf98194485d832ddfcd89c5b9b257
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-07-03 12:04:04 +0100

    move input schemas to a separate module

---
+++ src/server/api/schema/tasks.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod'
+import { Status } from '@prisma/client'
+
+export const getOneSchema = z.object({
+	id: z.string(),
+})
+
+const StatusEnum = z.nativeEnum(Status)
+
+export const updateOneSchema = z.object({
+	id: z.string(),
+	title: z.string(),
+	description: z.string().optional(),
+	status: StatusEnum,
+})
+
+export const createOneSchema = z.object({
+	title: z.string(),
+	description: z.string().optional(),
+	status: StatusEnum,
+})
+
+export type GetOneInput = z.TypeOf<typeof getOneSchema>
+export type UpdateOneInput = z.TypeOf<typeof updateOneSchema>
+export type CreateOneInput = z.TypeOf<typeof createOneSchema>
