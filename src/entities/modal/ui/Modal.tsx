cfeaa9fe1ac11fef748add222e4e9c55ecebf824
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-07-04 18:45:23 +0100

    add close buttton to modal

--- src/entities/modal/ui/Modal.tsx
+++ src/entities/modal/ui/Modal.tsx
@@ -1,6 +1,8 @@
 import { createPortal } from 'react-dom'
 
-import { Card } from '@/shared/ui-kit/Card'
+import { Button, Card } from '@/shared/ui-kit'
+
+import CloseIcon from '@/images/icons/close.svg'
 
 import { MODAL_ROOT_NODE_ID } from '../model'
 import { useCloseModal, useIsMounted, usePreventBodyScroll } from '../model'
@@ -22,7 +24,18 @@ export const Modal = ({ isOpen, onClose, children }: Props) => {
 				className={'fixed bottom-0 left-0 right-0 top-0 z-50 flex'}
 				onClick={onBackgroundClick}
 			>
-				<Card className={'m-auto'}>{children}</Card>
+				<Card className={'m-auto pt-16'}>
+					<Button
+						withEqualPaddings
+						size={'small'}
+						onClick={onClose}
+						className={'absolute right-4 top-4'}
+						viewType={'secondary'}
+					>
+						<CloseIcon />
+					</Button>
+					{children}
+				</Card>
 			</div>,
 			document.getElementById(MODAL_ROOT_NODE_ID) ?? document.body,
 		)
