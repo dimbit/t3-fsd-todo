efad83e899f5c8e1fd77ae05095b69a99f3d0562
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-07-04 18:12:04 +0100

    add button to open mobile sidebar to header

--- src/widgets/layouts/main-layout/ui/Header/Header.tsx
+++ src/widgets/layouts/main-layout/ui/Header/Header.tsx
@@ -1,10 +1,16 @@
 import { memo } from 'react'
+import clsx from 'clsx'
 
 import { useTaskModalStore } from '@/features/task-modal'
 
 import { Button } from '@/shared/ui-kit/Button'
 
-export const Header = memo(() => {
+import HamburgerIcon from '@/images/icons/hamburger.svg'
+
+type Props = {
+	openMobileSidebar: () => void
+}
+export const Header = memo(({ openMobileSidebar }: Props) => {
 	const openTaskCreationModal = useTaskModalStore.use.openTaskCreationModal()
 
 	const handleClickAddTask = () => {
@@ -12,7 +18,18 @@ export const Header = memo(() => {
 	}
 
 	return (
-		<div className={'flex h-14 w-full items-center justify-end px-4'}>
+		<div
+			className={clsx([
+				'col-start-1 col-end-3 row-start-1 row-end-2 flex h-14 w-full items-center justify-between px-4',
+				'sm:justify-end',
+			])}
+		>
+			<Button
+				className={'sm:hidden'}
+				onClick={openMobileSidebar}
+			>
+				<HamburgerIcon />
+			</Button>
 			<Button onClick={handleClickAddTask}>Add task</Button>
 		</div>
 	)
