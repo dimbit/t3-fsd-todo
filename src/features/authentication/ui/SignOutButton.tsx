9ba4f75997b20211bbb3e92bbc9a08b57f839940
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-07-04 15:16:28 +0100

    add icon to log out button

--- src/features/authentication/ui/SignOutButton.tsx
+++ src/features/authentication/ui/SignOutButton.tsx
@@ -1,15 +1,27 @@
 import { signOut } from 'next-auth/react'
+import clsx from 'clsx'
 
 import { Button } from '@/shared/ui-kit/Button'
 
-export const SignOutButton = () => {
+import LogOutButton from '@/images/icons/log-out.svg'
+
+type Props = {
+	withIcon?: boolean
+	withText?: boolean
+}
+export const SignOutButton = ({ withIcon, withText }: Props) => {
 	return (
 		<Button
 			onClick={() => {
 				void signOut()
 			}}
 		>
-			Sign out
+			{withIcon && <LogOutButton />}
+			{withText && (
+				<span className={clsx('text-ellipsis whitespace-nowrap')}>
+					Sign out
+				</span>
+			)}
 		</Button>
 	)
 }
