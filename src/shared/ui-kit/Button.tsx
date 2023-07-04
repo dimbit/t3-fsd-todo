827e2e03f64f395a55e44e1b401ba319f81c7ffe
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-07-04 15:16:59 +0100

    center content in button

--- src/shared/ui-kit/Button.tsx
+++ src/shared/ui-kit/Button.tsx
@@ -19,7 +19,7 @@ export const Button = forwardRef<HTMLButtonElement, Props>(
 			<button
 				ref={ref}
 				className={clsx([
-					'grid h-10 grid-flow-col items-center overflow-hidden rounded-md border px-3 py-2 shadow',
+					'grid h-10 grid-flow-col content-center items-center overflow-hidden rounded-md border px-3 py-2 shadow',
 					'border-neutral-200 bg-white  dark:border-neutral-500 dark:bg-neutral-700',
 					'disabled:cursor-not-allowed disabled:text-neutral-300',
 					'enabled:hover:bg-neutral-100 enabled:dark:hover:bg-neutral-600',
