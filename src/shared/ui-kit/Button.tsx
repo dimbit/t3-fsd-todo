bca028a1dfa2695aaab2a4a39707704019dfe0a3
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-07-04 18:44:13 +0100

    ad viewType and size properties to Button

--- src/shared/ui-kit/Button.tsx
+++ src/shared/ui-kit/Button.tsx
@@ -3,8 +3,13 @@ import clsx from 'clsx'
 
 import { Loader } from './Loader'
 
+type ViewType = 'primary' | 'secondary'
+type Size = 'small' | 'medium'
 type ButtonStyleProps = {
 	fullWidth: boolean
+	viewType?: ViewType
+	size?: Size
+	withEqualPaddings?: boolean
 }
 type Props = {
 	children?: React.ReactNode
@@ -14,15 +19,37 @@ type Props = {
 	React.ComponentProps<'button'>
 
 export const Button = forwardRef<HTMLButtonElement, Props>(
-	({ children, className, isLoading, fullWidth, ...props }, ref) => {
+	(
+		{
+			children,
+			className,
+			isLoading,
+			fullWidth,
+			viewType = 'primary',
+			size = 'medium',
+			withEqualPaddings,
+			...props
+		},
+		ref,
+	) => {
 		return (
 			<button
 				ref={ref}
 				className={clsx([
-					'grid h-10 grid-flow-col content-center items-center overflow-hidden rounded-md border px-3 py-2 shadow',
-					'border-neutral-200 bg-white  dark:border-neutral-500 dark:bg-neutral-700',
-					'disabled:cursor-not-allowed disabled:text-neutral-300',
-					'enabled:hover:bg-neutral-100 enabled:dark:hover:bg-neutral-600',
+					'grid grid-flow-col content-center items-center overflow-hidden',
+					size === 'small' && 'rounded px-2 py-1 text-sm',
+					size === 'medium' && 'h-10 rounded-md px-3 py-2 text-base',
+					viewType === 'primary' && [
+						'border border-neutral-200 bg-white shadow  dark:border-neutral-500 dark:bg-neutral-700',
+						'enabled:hover:bg-neutral-100 enabled:dark:hover:bg-neutral-600',
+					],
+					viewType === 'secondary' && [
+						'bg-white dark:bg-neutral-700',
+						'disabled:cursor-not-allowed disabled:text-neutral-300',
+						'enabled:hover:bg-neutral-100 enabled:dark:hover:bg-neutral-600',
+					],
+					withEqualPaddings && size === 'small' && 'px-1 py-1',
+					withEqualPaddings && size === 'medium' && 'px-2 py-2',
 					fullWidth && 'w-full',
 					className,
 				])}
