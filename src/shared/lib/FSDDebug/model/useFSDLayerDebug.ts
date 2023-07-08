446942365d0ac6ddba7096b89277007b6c95006a
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-07-08 18:01:02 +0100

    connect form state with FSD hook

--- src/shared/lib/FSDDebug/model/useFSDLayerDebug.ts
+++ src/shared/lib/FSDDebug/model/useFSDLayerDebug.ts
@@ -1,19 +1,24 @@
 import clsx from 'clsx'
 
+import { useFSDDebugStore } from './FSDDebugStore'
 import { type Layer } from './types'
 
 export const useFSDLayerDebug = (layer: Layer, name: string) => {
-	const colorUtilityPrefixies = ['border', 'after:text']
+	const colorUtilityPrefixies = ['outline', 'after:text']
 	const colorUtilities = colorUtilityPrefixies.map((prefix) => {
 		return `${prefix}-${layer}`
 	})
 	const path = `${layer}/${name}`
 
+	const isActive = useFSDDebugStore.use[layer]()
+
 	return {
 		['data-after-content']: path,
-		className: clsx([
-			'border after:absolute after:right-0 after:text-x after:content-[attr(data-after-content)]',
-			...colorUtilities,
-		]),
+		className: isActive
+			? clsx([
+					'outline after:absolute after:right-0 after:text-x after:content-[attr(data-after-content)]',
+					...colorUtilities,
+			  ])
+			: '',
 	}
 }
