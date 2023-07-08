4d384afbd5f5807982b78aa3866d407bf81f7b6d
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-07-08 12:20:47 +0100

    fix import

--- src/features/navigation/ui/NavigationBar.tsx
+++ src/features/navigation/ui/NavigationBar.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import clsx from 'clsx'
 
-import { navigation } from '../model/navigation'
+import { navigation } from '../model'
 
 type Props = {
 	isCollapsed?: boolean
