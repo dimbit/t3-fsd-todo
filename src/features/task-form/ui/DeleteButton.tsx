379f8d7fbccb3a6b3259b1ee9ccab12b5d2a6685
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-07-10 15:58:50 +0100

    remove FSD debug styles from delete task button

--- src/features/task-form/ui/DeleteButton.tsx
+++ src/features/task-form/ui/DeleteButton.tsx
@@ -1,23 +1,15 @@
 import clsx from 'clsx'
 
-import { useFSDLayerDebug } from '@/shared/lib/FSDDebug'
 import { Button } from '@/shared/ui'
 
 type Props = React.ComponentPropsWithoutRef<'button'>
 
 export const DeleteButton = ({ className, ...props }: Props) => {
-	const { className: debugClassName, ...rest } = useFSDLayerDebug(
-		'features',
-		DeleteButton.name,
-	)
-
 	return (
 		<Button
-			{...rest}
 			className={clsx([
 				'enabled:border-red-400 enabled:dark:border-red-400',
 				className,
-				debugClassName,
 			])}
 			{...props}
 		>
