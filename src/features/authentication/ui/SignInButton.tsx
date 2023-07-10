d1e858647601f7940159a35324fdb8d9686561f9
Dmitrii Goriachev <dimbit.access@gmail.com>
2023-07-10 15:58:33 +0100

    remov FSD debug styles from sign in button

--- src/features/authentication/ui/SignInButton.tsx
+++ src/features/authentication/ui/SignInButton.tsx
@@ -1,18 +1,10 @@
 import { signIn } from 'next-auth/react'
 
-import { useFSDLayerDebug } from '@/shared/lib/FSDDebug'
 import { Button } from '@/shared/ui'
 
 export const SignInButton = () => {
-	const { className: debugClassName, ...rest } = useFSDLayerDebug(
-		'features',
-		SignInButton.name,
-	)
-
 	return (
 		<Button
-			{...rest}
-			className={debugClassName}
 			onClick={() => {
 				void signIn()
 			}}
